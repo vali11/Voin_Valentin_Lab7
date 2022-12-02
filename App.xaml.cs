1535a3e5af424b459a6e1d7290266a395207ab39
valiv <valentin.voin@stud.ubbcluj.ro>
2022-12-02 22:05:21 +0200

    lab8final

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,8 +1,27 @@
-﻿namespace Voin_Valentin_Lab7;
+﻿using System;
+using Voin_Valentin_Lab7.Data;
+using System.IO;
+
+namespace Voin_Valentin_Lab7;
 
 public partial class App : Application
 {
-	public App()
+    static ShoppingListDatabase database;
+    public static ShoppingListDatabase Database
+    {
+        get
+        {
+            if (database == null)
+            {
+                database = new
+               ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+               LocalApplicationData), "ShoppingList.db3"));
+            }
+            return database;
+        }
+    }
+
+    public App()
 	{
 		InitializeComponent();
 
---
+++ ListEntryPage.xaml.cs
@@ -0,0 +1,33 @@
+using Voin_Valentin_Lab7.Models;
+
+namespace Voin_Valentin_Lab7;
+
+public partial class ListEntryPage : ContentPage
+{
+	public ListEntryPage()
+	{
+		InitializeComponent();
+	}
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        listView.ItemsSource = await App.Database.GetShopListsAsync();
+    }
+    async void OnShopListAddedClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ListPage{
+            BindingContext = new ShopList()
+        });
+    }
+    async void OnListViewItemSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+        if (e.SelectedItem != null)
+        {
+            await Navigation.PushAsync(new ListPage{
+                BindingContext = e.SelectedItem as ShopList
+            });
+        }
+    }
+
+}
